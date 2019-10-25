899278bcb926865e39a281ef05ff9e02f536bc53
bchavez <bchavez@bitarmory.com>
2019-10-24 23:32:09 -0700

    MemoryLeak and OutOfMemory done.

--- Source/Havoc/Memory.cs
+++ Source/Havoc/Memory.cs
@@ -1,10 +1,54 @@
-﻿namespace Havoc
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Havoc
 {
    public class Memory : Scenario
    {
       public void StackOverflow()
       {
          StackOverflow();
+      }
+
+      public Task<Void> MemoryLeak(int allocationSize, TimeSpan allocationDelay, CancellationToken cancellationToken = default)
+      {
+         var list = new List<byte[]>();
+
+         this.SetupDedicatedThread(out var thread, out var tcs, Start, nameof(MemoryLeak));
+
+         thread.Start();
+
+         return tcs.Task;
+
+
+         void Start()
+         {
+            using( cancellationToken.Register(CleanUp) )
+            {
+               while( !cancellationToken.IsCancellationRequested )
+               {
+                  list.Add(new byte[allocationSize]);
+
+                  if ( allocationDelay != TimeSpan.Zero )
+                  {
+                     Thread.Sleep(allocationDelay);
+                  }
+               }
+            }
+         }
+
+         void CleanUp()
+         {
+            list.Clear();
+         }
+      }
+
+      public void OutOfMemory()
+      {
+         //1GB allocation, as fast as possible.
+         MemoryLeak(1024 * 1024 * 1024, TimeSpan.Zero);
       }
    }
 }
