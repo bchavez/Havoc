46def90ddbad2a59b1947e202ef9b4b33b2038dd
bchavez <bchavez@bitarmory.com>
2019-10-26 14:37:11 -0700

    enable remote rdp

--- Source/Havoc/Properties/AssemblyInfo.cs
+++ Source/Havoc/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Bit Armory Inc © 2019")]
 [assembly: AssemblyVersion("0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0 built on 10/26/2019 6:04:44 PM")]
+[assembly: AssemblyInformationalVersion("0.0.0 built on 10/26/2019 8:44:24 PM")]
 [assembly: AssemblyTrademark("MIT License")]
 [assembly: AssemblyDescription("https://github.com/bchavez/Havoc")]
 [assembly: InternalsVisibleTo("Havoc.Tests")]
@@ -21,7 +21,7 @@ namespace System {
         internal const System.String AssemblyCopyright = "Bit Armory Inc © 2019";
         internal const System.String AssemblyVersion = "0.0.0";
         internal const System.String AssemblyFileVersion = "0.0.0";
-        internal const System.String AssemblyInformationalVersion = "0.0.0 built on 10/26/2019 6:04:44 PM";
+        internal const System.String AssemblyInformationalVersion = "0.0.0 built on 10/26/2019 8:44:24 PM";
         internal const System.String AssemblyTrademark = "MIT License";
         internal const System.String AssemblyDescription = "https://github.com/bchavez/Havoc";
         internal const System.String InternalsVisibleTo = "Havoc.Tests";
