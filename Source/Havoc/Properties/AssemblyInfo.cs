c4a0b6b02e3c32c59c634c4990a016fe12c64d01
bchavez <bchavez@bitarmory.com>
2019-11-02 19:08:32 -0700

    Run clean

--- Source/Havoc/Properties/AssemblyInfo.cs
+++ Source/Havoc/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Havoc chaos-engineering for .NET")]
 [assembly: AssemblyProduct("Havoc")]
 [assembly: AssemblyCompany("Brian Chavez")]
-[assembly: AssemblyCopyright("Bit Armory Inc © 2019")]
+[assembly: AssemblyCopyright("Bit Armory Inc © 2015")]
 [assembly: AssemblyVersion("0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0 built on 10/26/2019 8:44:24 PM")]
+[assembly: AssemblyInformationalVersion("0.0.0-localbuild built on 1/1/2015 12:00:00 AM")]
 [assembly: AssemblyTrademark("MIT License")]
 [assembly: AssemblyDescription("https://github.com/bchavez/Havoc")]
 [assembly: InternalsVisibleTo("Havoc.Tests")]
@@ -18,10 +18,10 @@ namespace System {
         internal const System.String AssemblyTitle = "Havoc chaos-engineering for .NET";
         internal const System.String AssemblyProduct = "Havoc";
         internal const System.String AssemblyCompany = "Brian Chavez";
-        internal const System.String AssemblyCopyright = "Bit Armory Inc © 2019";
+        internal const System.String AssemblyCopyright = "Bit Armory Inc © 2015";
         internal const System.String AssemblyVersion = "0.0.0";
         internal const System.String AssemblyFileVersion = "0.0.0";
-        internal const System.String AssemblyInformationalVersion = "0.0.0 built on 10/26/2019 8:44:24 PM";
+        internal const System.String AssemblyInformationalVersion = "0.0.0-localbuild built on 1/1/2015 12:00:00 AM";
         internal const System.String AssemblyTrademark = "MIT License";
         internal const System.String AssemblyDescription = "https://github.com/bchavez/Havoc";
         internal const System.String InternalsVisibleTo = "Havoc.Tests";
